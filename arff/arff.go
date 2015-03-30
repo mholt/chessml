07564387c481c4d93845f463b908f1128ea8189a
willpalm <will5ryanpalmer@gmail.com>
2015-03-30 13:40:36 -0600

    completed the generateARFF function

--- arff/arff.go
+++ arff/arff.go
@@ -23,8 +23,20 @@ func GenerateARFF(games []Game, numMoves int) {
 	f.WriteString("@attribute attack-value   REAL\n")
 	f.WriteString("@attribute mobility       REAL\n")
 	f.WriteString("@attribute space          REAL\n")
-	f.WriteString("@attribute class          {won, lost, draw, other}\n\n")
+	f.WriteString("@attribute class          {1-0, 0-1, 1/2-1/2, *}\n\n")
 	f.WriteString("@data\n%%\n%% " + FormatInt(len(games), 10) + " instances\n%%\n")
+
+	for i := 0; i < len(games); i++ {
+		games[i].Execute(numMoves)
+
+		material = Material(games[i], WhiteTeam)
+		attackValue = AttackValue(games[i], WhiteTeam)
+		mobility = Mobility(games[i], WhiteTeam)
+		space = Space(games[i], WhiteTeam)
+		outcome = games[i].Tags["Result"]
+
+		f.WriteString(FormatInt(material, 10) + "," + FormatInt(attackValue, 10) + "," + FormatInt(mobility, 10) + "," + FormatInt(space, 10) + "," + outcome)
+	}
 
 	f.WriteString("%%\n%%\n%%\n")
 
